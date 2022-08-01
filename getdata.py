28018648a6f27822f5aebedca208e554311b9938
Saptarshi Dutta <quantumcosmos7@gmail.com>
2022-08-01 12:29:44 +0530

    FIRST COMMIT

---
+++ getdata.py
@@ -0,0 +1,21 @@
+import json
+import requests
+from decouple import config
+API_KEY = config('MAL_API_KEY')
+def getdata(username):
+    headers = {
+        'Authorization': 'Bearer ' + API_KEY,
+    }
+    response = requests.get(
+        url="https://api.myanimelist.net/v2/users/" + username + "/animelist?offset=0&limit=25&status=watching&sort=list_updated_at", headers=headers)
+    data = response.json()
+    with open(username + ".json", 'w') as file:
+            json.dump(data, file, indent=4)
+            print('data saved')
+
+    image_urls = []
+
+    for node in data["data"]:
+        image_urls.append(node["node"]["main_picture"]["large"])
+
+    return image_urls
---
+++ img_gen.py
@@ -0,0 +1,43 @@
+from math import ceil
+from PIL import Image
+from io import BytesIO
+import requests
+from getdata import getdata
+from get_img_url import get_urls
+
+def get_images(urls):
+  r = [requests.get(url) for url in urls]
+  print(len(r), "Images loaded")
+  images = [Image.open(BytesIO(x.content)).resize((400, 600)) for x in r]
+  widths, heights = zip(*(i.size for i in images))
+  return images
+
+def create_canvas(url_len):
+  total_width = 400*min(5, url_len)
+  max_height = 600*ceil(url_len/5)
+
+  new_im = Image.new('RGBA', (total_width, max_height))
+  return new_im, total_width
+
+
+def build_collage(images, total_width):
+  x_offset = y_offset = 0
+  for im in images:
+    if x_offset == total_width:
+      x_offset = 0
+      y_offset += im.size[1]
+    new_im.paste(im, (x_offset, y_offset))
+    x_offset += im.size[0]
+  return new_im
+
+inp = input().split(' ')
+username = inp[0]
+urls = getdata(username)
+
+new_im, total_width = create_canvas(len(urls))
+images = get_images(urls)
+new_image = build_collage(images, total_width)
+print("Collage building compleat")
+
+new_im.save(username + ".png")
+print("Image Saved")
